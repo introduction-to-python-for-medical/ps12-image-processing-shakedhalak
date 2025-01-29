a024e7866579e3c2a7d2b5d6788423c0d0655129
shakedhalak <shakedhalak@gmail.com>
2025-01-29 16:11:17 +0200

    Update image_utils.pynew

--- image_utils.py
+++ image_utils.py
@@ -8,32 +8,30 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 def load_image(file_path):
-  
     image = Image.open(file_path)
     image_array = np.array(image)
     return image_array 
 
-file_path = '/mypicture'  
-image_array = load_image(file_path)
+load_image('/argen.jpg')
 
-plt.imshow(image_array, cmap='gray')
+from scipy.signal import convolve2d
+import numpy as np
 
 def edge_detection(image_array):
    
-    grayscale_image = np.mean(image_array, axis=2)
+    gray_image = np.mean(image_array, axis=2)
 
     kernelY = np.array([
-        [1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1]]) 
+       [1, 2, 1],
+       [0, 0, 0],
+       [-1, -2, -1]]) 
      
-   kernelX = np.array([
+    kernelX = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1,0, 1] ])
-    edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
-    edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill', fillvalue=0)
 
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
-
-    return edgeMAG
+  
